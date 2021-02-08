f7aceefe144536f12f0f18cd3b61305ee7e3eb04
boalbert <andersson.albert@gmail.com>
2021-02-09 00:29:55 +0100

    Removed deprecated filehandler method

--- plugin/src/main/java/se/iths/plugin/FileIMPL.java
+++ plugin/src/main/java/se/iths/plugin/FileIMPL.java
@@ -11,20 +11,6 @@ import java.nio.file.Paths;
 
 public class FileIMPL implements IOhandler {
 
-	public static byte[] readFromFile(File file) {
-		byte[] content = new byte[0];
-		System.out.println("Does file exists: " + file.exists());
-		if (file.exists() && file.canRead()) {
-			try (FileInputStream fileInputStream = new FileInputStream(file)) {
-				content = new byte[(int) file.length()];
-				int count = fileInputStream.read(content);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return content;
-	}
-
 	@Override
 	public String urlHandler(String requestPath, Socket socket) throws IOException {
 
